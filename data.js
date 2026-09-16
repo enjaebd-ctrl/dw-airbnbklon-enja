8266f7887c16d47061883b24e2cb77fc2d1692e5
Enja <enjaebd@gmail.com>
2026-09-16 08:32:50 +0200

    more siden er lavet

--- data.js
+++ data.js
@@ -2,8 +2,50 @@ const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 
 fetch(`data/${id}.json`)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
+    .then(response => response.json())
+    .then(data => {
 
-  });
+        const destination = document.querySelector("#destination");
+
+        destination.innerHTML = `
+            
+            <div class="destination-page">
+
+                <img 
+                    class="destination-image"
+                    src="img/${data.image}" 
+                    alt="${data.title}"
+                >
+
+                <div class="destination-info">
+
+                    <p class="destination-country">
+                        ${data.destination}
+                    </p>
+
+                    <h1>${data.title}</h1>
+
+                    <h2>${data.subtitle}</h2>
+
+                    <p class="description">
+                        ${data.text}
+                    </p>
+
+                    <h3>Facilities</h3>
+
+                    <ul>
+                        ${data.facilities.map(facility => `
+                            <li>${facility}</li>
+                        `).join("")}
+                    </ul>
+
+                
+
+                </div>
+
+            </div>
+        `;
+    })
+    .catch(error => {
+        console.error("Der skete en fejl:", error);
+    });
